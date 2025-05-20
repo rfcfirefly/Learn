f301f1fbafc2fcfc466a99dea1a5cf54ed1ad37c
rfcfirefly <rfcfirefly@users.noreply.github.com>
2025-05-20 12:55:19 +0100

    Update swap.c

--- src/swap.c
+++ src/swap.c
@@ -1,29 +1,36 @@
-// Basic c program: showing functions and use of pointers
-//
+/*
+ * Basic c program: showing functions and use of pointers.
+ * Swapping 2 integer values via a function.
+ *
+*/
 #include <stdio.h>
 
-void swap(int* x, int* y)
-{
-    printf("Swap: %i and %i\n", *x, *y);
-    int tmp = *x;
-    *x = *y;
-    *y = tmp;
-}
+void swap(int*, int*);
+
 
 int main(void)
 {
-    // Final Values: x:10, y:90
     int x = 10;
     int y = 90;
 
     printf("x is: %i\n", x);
     printf("y is: %i\n", y);
 
-    // swapping
     swap(&x, &y);
 
     printf("x is now: %i\n", x);
     printf("y is now: %i\n", y);
 
+
     return 0;
+}
+
+
+void swap(int* x, int* y)
+/* Swap the values of 2 integers */
+{
+    printf("Swap: %i and %i\n", *x, *y);
+    int tmp = *x;
+    *x = *y;
+    *y = tmp;
 }
